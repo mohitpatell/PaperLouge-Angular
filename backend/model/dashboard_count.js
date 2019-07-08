ec8ea3005154df0d0a30562341407b14cc990384
mohitpatel787 <mohitpatel9753@gmail.com>
2019-07-09 01:01:50 +0530

    Unnessary things removed

--- backend/model/dashboard_count.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose');
-
-const count=mongoose.Schema({
-    user:{type:Number},
-    books:{type:Number},
-    downloads:{type:Number}
-})
-
-module.exports=mongoose.model("TotalCount",count);
